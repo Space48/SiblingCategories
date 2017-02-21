f2cb05f4a605149befaf2afd809a9df80d16382a
Raul E Watson <rdiaz@Rauls-MacBook-Pro.local>
2017-02-21 16:33:26 +0000

    Add Layout and block

---
+++ src/Block/SiblingCategories.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Space48\SiblingCategories\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class SiblingCategories extends Template
+{
+
+}
+
