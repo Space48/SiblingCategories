4035df649923513420ee9fa10bb5bd1abd451c08
Raul E Watson <rdiaz@Rauls-MacBook-Pro.local>
2017-02-22 13:55:38 +0000

    Show block content in sidebar main

--- src/Block/SiblingCategories.php
+++ src/Block/SiblingCategories.php
@@ -4,10 +4,24 @@ declare(strict_types = 1);
 
 namespace Space48\SiblingCategories\Block;
 
+use Magento\Catalog\Block\Navigation;
 use Magento\Framework\View\Element\Template;
 
 class SiblingCategories extends Template
 {
 
+    protected $_navigation;
+
+
+    public function getCategory()
+    {
+        return $this->_navigation->getCategory();
+    }
+
+    protected function _construct(Navigation $navigation)
+    {
+        parent::_construct();
+        $this->_navigation = $navigation;
+    }
 }
 
